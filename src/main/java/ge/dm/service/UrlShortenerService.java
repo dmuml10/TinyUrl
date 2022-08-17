e651a8d254b7d3c71bb134920b4e4bb4de92829b
dmuml10 <dmuml10@gmail.com>
2022-08-17 14:57:57 +0300

    first commit

---
+++ src/main/java/ge/dm/service/UrlShortenerService.java
@@ -0,0 +1,13 @@
+package ge.dm.service;
+
+public class UrlShortenerService {
+
+    public String shortenUrl(String url) {
+        return null;
+    }
+
+    public String getOriginalUrl(String urlHash) {
+        return null;
+    }
+
+}
